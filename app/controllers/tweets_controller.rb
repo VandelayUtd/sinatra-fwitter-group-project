84315bccbd404877594b1260e09344c00117ed17
VandelayUtd <drewhadley.guitar@gmail.com>
2021-07-23 15:41:19 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,6 +2,7 @@ class TweetsController < ApplicationController
 
     get '/tweets' do
         if logged_in?
+            
             @tweets = Tweet.all 
             @user = current_user
             erb :"tweets/tweets"
