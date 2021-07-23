29e3ab7add1d2a94e0db27e5ad1e2ba2efe87812
VandelayUtd <drewhadley.guitar@gmail.com>
2021-07-23 12:51:39 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,43 @@
 class UsersController < ApplicationController
 
+    get '/signup' do
+        
+        if !logged_in?
+            erb :"users/create_user"
+        else
+        redirect "/tweets"
+        end
+    end
+    
+    post "/signup" do
+        
+        if params[:username] == "" || params[:password] == "" || params[:email] == ""
+            redirect to "/signup"
+        else
+        @user = User.create(params)
+        @user.save
+        session[:user_id] = @user.id
+        redirect to "/tweets"
+        end
+    end
+
+    get '/login' do
+        if !logged_in?
+        erb :"users/login"
+        else
+        redirect to "/tweets"
+        end
+    end
+
+    post '/login' do
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect to "/tweets"
+        else 
+            redirect to "/signup"
+        end
+        
+    end    
 
 end
