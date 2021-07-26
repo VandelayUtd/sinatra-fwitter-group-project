2e4f45d4d82114af081a38644d416c3e66856137
VandelayUtd <drewhadley.guitar@gmail.com>
2021-07-26 12:07:00 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,8 @@
 class TweetsController < ApplicationController
 
     get '/tweets' do
-        if logged_in?
-            
+    #   binding.pry
+        if logged_in?    
             @tweets = Tweet.all 
             @user = current_user
             erb :"tweets/tweets"
@@ -11,13 +11,18 @@ class TweetsController < ApplicationController
         end
     end
 
-    get '/tweets/new' do 
+    get '/tweets/new' do
+        
         if logged_in?
             @user = current_user
             erb :"tweets/new"
         else 
             redirect to "/login"
         end
+    end
+
+    post '/tweets' do
+        
     end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,13 +31,14 @@ class UsersController < ApplicationController
     post '/login' do
         @user = User.find_by(username: params[:username])
         if @user && @user.authenticate(params[:password])
-            binding.pry
             session[:user_id] = @user.id
+            
             redirect to "/tweets"
         else 
             redirect to "/signup"
         end    
     end
+
 
     get '/logout' do 
         if logged_in?
